bc7f8fb81791525106dc02e42e638306b470fae9
Javi Ortiz <javiortizinthewall@gmail.com>
2024-04-01 19:07:06 +0200

    added port forwd from docker to host

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -v /home/pablo/tempsbus:/app --restart always tempsbus --name tempsbus
+docker run -d -v /home/pablo/tempsbus:/app --restart always tempsbus --name tempsbus -p 5000:5000
