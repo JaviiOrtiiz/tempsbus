8f84d1c6ef756c88d9255364bfaf9900dcf4368c
Javi Ortiz <javiortizinthewall@gmail.com>
2024-05-24 14:58:09 +0200

    code was wrong xd fixed the memory limitation

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -v /home/pablo/tempsbus:/app --restart always --name tempsbus -p 5000:5000 tempsbus --memory=50m
+docker run -d -v /home/pablo/tempsbus:/app --restart always --memory=50m --name tempsbus -p 5000:5000 tempsbus
