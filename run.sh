4f6853fcdd24ef8c6af617e7143e0dc694c6f5b1
Javi Ortiz <javiortizinthewall@gmail.com>
2024-05-24 14:55:47 +0200

    added memory limit of 50m to run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -v /home/pablo/tempsbus:/app --restart always --name tempsbus -p 5000:5000 tempsbus
+docker run -d -v /home/pablo/tempsbus:/app --restart always --name tempsbus -p 5000:5000 tempsbus --memory=50m
